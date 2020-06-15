7dd1385c6830cb91410ec173de350cba94bc3991
pichamon <pichamon@PICHAMON0N018>
2020-06-15 10:54:33 +0700

    Route: Mins to Hours

--- admin/route.php
+++ admin/route.php
@@ -58,7 +58,7 @@
                                             "<td>" . $row["Name"] . "</td>" .
                                             "<td>" . $row["Begin"] . "</td>" .
                                             "<td>" . $row["Destination"] . "</td>" .
-                                            "<td>" . $row["Usagetime"] . " นาที</td>" .
+                                            "<td>" . $row["Usagetime"] . " ชั่วโมง</td>" .
                                             "<td>" . $row["Price"] . " บาท</td>" .
                                             "<td>" . $row["Description"] . "</td>" .
                                             "<td align=\"center\">
